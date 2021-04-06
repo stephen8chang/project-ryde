d4ea141fcc6416e38e6409302da47f26a16b8f70
Harish Rajanala <harishrajanala@utexas.edu>
2021-04-05 21:42:33 -0500

    hehe p5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [getMessage, setGetMessage] = useState({})
 
   useEffect(()=>{
-    axios.get('https://project-ryde.herokuapp.com/').then(response => {
+    axios.get('https://project-ryde.herokuapp.com/', {mode: 'no-cors'}).then(response => {
       console.log("SUCCESS", response)
       setGetMessage(response)
     }).catch(error => {
