21b5d1144505de43d82cc9519f00e37aef38b19c
Harish Rajanala <harishrajanala@utexas.edu>
2021-04-07 14:51:06 -0500

    node back end working with front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import axios from 'axios';
 import LoginScreen from './screens/LoginScreen';
@@ -7,7 +7,22 @@ import RegisterScreen from './screens/RegisterScreen';
 import DashboardScreen from './screens/DashboardScreen';
 import NavBar from './components/NavBar';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {apiResponse: ""}
+  }
+
+  callAPI() { 
+    fetch("http://localhost:9000/testAPI")
+    .then(res => res.text())
+    .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+  
   // const [getMessage, setGetMessage] = useState({});
 
   // useEffect(() => {
@@ -28,19 +43,21 @@ function App() {
   //   //   console.log(error)
   //   // })
   // }, []);
-
-  return (
-    <div className='container'>
-      <NavBar />
-      <BrowserRouter>
-        <div className='container'>
-          <Route exact path='/' component={DashboardScreen} />
-          <Route path='/register' component={RegisterScreen} />
-          <Route path='/login' component={LoginScreen} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+  render() {
+    return (
+      <div className='container'>
+        <NavBar />
+        <BrowserRouter>
+          <div className='container'>
+            <Route exact path='/dash' component={DashboardScreen} />
+            <Route path='/register' component={RegisterScreen} />
+            <Route path='/login' component={LoginScreen} />
+          </div>
+        </BrowserRouter>
+        <p className="App-intro">;{this.state.apiResponse}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
