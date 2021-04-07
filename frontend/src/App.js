8069394e7f5bc7bf2ce36a240ce04627502005cc
Norman Wang <normanwang@utexas.edu>
2021-04-06 21:06:31 -0500

    Added login and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,45 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
-import axios from 'axios'
-import LoginScreen from './components/screens/LoginScreen';
-import RegisterScreen from './components/screens/RegisterScreen'
-import DashboardScreen from './components/screens/DashboardScreen'
+import { BrowserRouter, Route } from 'react-router-dom';
+import axios from 'axios';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import DashboardScreen from './screens/DashboardScreen';
+import NavBar from './components/NavBar';
 
 function App() {
-  const [getMessage, setGetMessage] = useState({})
+  // const [getMessage, setGetMessage] = useState({});
 
-  useEffect(()=>{
-    axios.get('https://project-ryde.herokuapp.com/', {mode: 'no-cors'}).then(response => {
-      console.log("SUCCESS", response)
-      setGetMessage(response)
-    }).catch(error => {
-      console.log(error)
-    })
+  // useEffect(() => {
+  //   axios
+  //     .get('https://project-ryde.herokuapp.com/', { mode: 'no-cors' })
+  //     .then(response => {
+  //       console.log('SUCCESS', response);
+  //       setGetMessage(response);
+  //     })
+  //     .catch(error => {
+  //       console.log(error);
+  //     });
 
-    // axios.get('http://localhost:5000/flask/hello').then(response => {
-    //   console.log("SUCCESS", response)
-    //   setGetMessage(response)
-    // }).catch(error => {
-    //   console.log(error)
-    // })
-  }, [])
+  //   // axios.get('http://localhost:5000/flask/hello').then(response => {
+  //   //   console.log("SUCCESS", response)
+  //   //   setGetMessage(response)
+  //   // }).catch(error => {
+  //   //   console.log(error)
+  //   // })
+  // }, []);
 
   return (
-    <div className="container">
-    <BrowserRouter>
-      <div className='container'>
-        {/* <Header /> */}
-        <Route exact path="/" component={DashboardScreen} />
-        <Route path="/register" component={RegisterScreen} />
-        <Route path="/login" component={LoginScreen} />
-      </div>
-    </BrowserRouter>
-  </div>
+    <div className='container'>
+      <NavBar />
+      <BrowserRouter>
+        <div className='container'>
+          <Route exact path='/' component={DashboardScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/login' component={LoginScreen} />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
