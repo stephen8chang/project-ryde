cf4b54ad39945f0a3545df9c67ad629b8aab1e29
Stephen <stephen8chang@gmail.com>
2021-04-08 19:21:00 -0500

    some UI for projects page

--- frontend/src/screens/ProjectsScreen.js
+++ frontend/src/screens/ProjectsScreen.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 const ProjectsScreen = () => {
-  return <div>Projects</div>;
+  return (
+    <>
+      <div>
+        <big>Welcome to the Projects Page! </big>
+      </div>
+      <div>
+        <big>Here you can create or search through existing projects.</big>
+      </div>
+      <div>
+        <button>Create Project</button>
+      </div>
+      <br></br>
+      <table border="1px solid black">
+        <tr>
+          <th>Name</th><br></br>
+          <th>ID</th><br></br>
+          <th>Description</th><br></br>
+          <th>Link to Open</th><br></br>
+        </tr>
+      </table>
+    </>
+  );
 };
 
 export default ProjectsScreen;
