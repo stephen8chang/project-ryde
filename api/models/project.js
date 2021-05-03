bd9578d463234361428d8f0210a1351bf51d3c03
yashdhaduti <yashdhaduti@utexas.edu>
2021-05-03 15:57:07 -0500

    dash

--- api/models/project.js
+++ api/models/project.js
@@ -5,12 +5,8 @@ const projectSchema = new Schema({
   creator: String,
   projectName: String,
   description: String,
-  funds: Number,
   checkedOut: [
     { type: mongoose.Schema.Types.ObjectId, ref: 'checkedOut', default: [] }
-  ],
-  projectUsers: [
-    { type: mongoose.Schema.Types.ObjectId, ref: 'users', default: [] }
   ],
   access: Boolean
 });
