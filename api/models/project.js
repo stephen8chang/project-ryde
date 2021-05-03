44962dad7ca1f25fd6b07e1dca898fcf7fbd05e0
Norman Wang <normanwang@utexas.edu>
2021-05-03 17:01:30 -0500

    Added project.js

--- api/models/project.js
+++ api/models/project.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 const { Schema } = mongoose;
-const projectSchema = new Schema({
-  name: String,
-  description: String,
-  id: Number
-});
-projectSchema.pre('save', async function(next) {
 
-  next();
+const projectSchema = new Schema({
+  creator: String,
+  projectName: String,
+  description: String,
+  HW1Amt: Number,
+  HW2Amt: Number,
+  access: Boolean
 });
-const User = mongoose.model('projects', projectSchema);
-module.exports = Projects;
+
+const Project = mongoose.model('projects', projectSchema);
+module.exports = Project;
